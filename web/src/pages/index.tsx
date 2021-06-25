6a903e50a80c81bc6151fe62973ca9105dcb5f91
Allyedge <66177622+Allyedge@users.noreply.github.com>
2021-06-25 11:57:49 +0200

    web / refactor(index): clean up index.tsx

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,15 +1,4 @@
-import { useEffect } from "react";
-import { useLogoutMutation, useMeQuery } from "../generated/graphql";
-
 const Index = () => {
-  const { data = {} } = useMeQuery();
-
-  const [logout, _] = useLogoutMutation();
-
-  useEffect(() => {
-    console.log(data.me);
-  }, []);
-
   return (
     <div className="w-full h-full grid grid-cols-3 gap-4 text-center">
       <div>
@@ -17,19 +6,6 @@ const Index = () => {
       </div>
       <div>
         <h2>Center</h2>
-        <h2>{data.me!.username}</h2>
-        <a
-          className="text-lg"
-          href=""
-          onClick={async (e) => {
-            e.preventDefault();
-            const data = await logout();
-            console.log(data);
-            return location.reload();
-          }}
-        >
-          Click to logout
-        </a>
       </div>
       <div>
         <h2>Right</h2>
