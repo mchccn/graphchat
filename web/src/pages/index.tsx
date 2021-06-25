21151c56af2c205e0b5fa75ec83b818ea9ca135c
Allyedge <66177622+Allyedge@users.noreply.github.com>
2021-06-25 11:37:02 +0200

    web / feat(web): add basic grid layout & update readme

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -11,26 +11,29 @@ const Index = () => {
   }, []);
 
   return (
-    <div className="w-full h-full grid place-items-center text-center">
-      {data.me ? (
-        <div>
-          <h1>{data.me.username}</h1>
-          <a
-            className="text-lg"
-            href=""
-            onClick={async (e) => {
-              e.preventDefault();
-              const data = await logout();
-              console.log(data);
-              return location.reload();
-            }}
-          >
-            Click to logout
-          </a>
-        </div>
-      ) : (
-        <div></div>
-      )}
+    <div className="w-full h-full grid grid-cols-3 gap-4 text-center">
+      <div>
+        <h2>Left</h2>
+      </div>
+      <div>
+        <h2>Center</h2>
+        <h2>{data.me!.username}</h2>
+        <a
+          className="text-lg"
+          href=""
+          onClick={async (e) => {
+            e.preventDefault();
+            const data = await logout();
+            console.log(data);
+            return location.reload();
+          }}
+        >
+          Click to logout
+        </a>
+      </div>
+      <div>
+        <h2>Right</h2>
+      </div>
     </div>
   );
 };
